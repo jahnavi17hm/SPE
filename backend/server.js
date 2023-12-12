637091561b0ff2ce0e57534a0b6d66293ceae176
jahnavi17hm <jahnavi.challa@iiitb.ac.in>
2023-12-12 14:43:07 +0530

    14

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 require("dotenv").config();
 const port = process.env.PORT || 5000
-const dbConnection = require('./db')
+// const dbConnection = require('./db')
 
 app.use(cors({
     origin: '*',
