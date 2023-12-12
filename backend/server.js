db4f027c8113ffa536d7884109bb6bdfe566601f
jahnavi17hm <jahnavi.challa@iiitb.ac.in>
2023-12-12 14:31:16 +0530

    13

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authorize = require("./middleware/tokenAuth");
-
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
 
 const app = express();
-const port = process.env.PORT || 5001;
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+require("dotenv").config();
+const port = process.env.PORT || 5000
+const dbConnection = require('./db')
 
 app.use(cors({
-  origin: '*',
-  methods: ['POST', 'GET', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.use(express.json());
+    origin: '*',
+    methods: '*',   
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Methods: *');
+    res.header('Access-Control-Allow-Headers: *');
+    next();
+});
 
 const uri = process.env.ATLAS_URI;
 
