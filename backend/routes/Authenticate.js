0a13b86c90da5e0f3d4754fb8a0c6cfb40023b7a
jahnavi17hm <149154020+jahnavi17hm@users.noreply.github.com>
2023-12-14 11:58:04 +0530

    Update Authenticate.js

--- backend/routes/Authenticate.js
+++ backend/routes/Authenticate.js
@@ -1,151 +1,199 @@
 const router = require('express').Router();
-let bcrypt = require("bcrypt")
-let moment = require('moment');
-let Buyer = require('../models/Buyer');
-let Vendor = require('../models/Vendor');
-let jwt = require("jsonwebtoken");
-const { OAuth2Client } = require('google-auth-library')
+const bcrypt = require("bcrypt");
+const moment = require('moment');
+const Buyer = require('../models/Buyer');
+const Vendor = require('../models/Vendor');
+const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require('google-auth-library');
+const logger = require('../logger/log');
+
 require('dotenv').config();
-const client = new OAuth2Client(process.env.GOOG_ID)
+const client = new OAuth2Client(process.env.GOOG_ID);
+
 router.route("/google").post(async (req, res) => {
-    const token = req.body.token;
-    const ticket = await client.verifyIdToken({ idToken: token, audience: process.env.GOOG_ID });
-    const email = ticket.getPayload().email;
-    let buyer = await Buyer.findOne({email: email});
-    let vendor = await Vendor.findOne({email: email});
-    if (!buyer && !vendor) {
+    try {
+        const token = req.body.token;
+        const ticket = await client.verifyIdToken({ idToken: token, audience: process.env.GOOG_ID });
+        const email = ticket.getPayload().email;
+        let buyer = await Buyer.findOne({ email: email });
+        let vendor = await Vendor.findOne({ email: email });
+        if (!buyer && !vendor) {
+            logger.error("Google login failed - No account with this email exists", { email: email });
+            return res.status(200).json({
+                status: 1,
+                error: "No account with this email exists"
+            });
+        }
+        let tokenx;
+        if (buyer) {
+            tokenx = {
+                email: email,
+                type: "buyer"
+            };
+        }
+        else {
+            tokenx = {
+                email: email,
+                type: "vendor"
+            };
+        }
+        const signedtoken = jwt.sign(tokenx, process.env.SECRET);
+        logger.info("Google login successful");
         return res.status(200).json({
+            status: 0,
+            usertoken: signedtoken
+        });
+    } catch (error) {
+        logger.error("Google login failed", { error: error.message });
+        return res.status(500).json({
             status: 1,
-            error : "No account with this email exists"
-        })
+            error: error.message
+        });
     }
-    let tokenx;
-    if (buyer) {
-        tokenx = {
-            email: email,
-            type: "buyer"
-        } 
-    }
-    else {
-        tokenx = {
-            email: email,
-            type: "vendor"
-        } 
-    }
-    const signedtoken = jwt.sign(tokenx, process.env.SECRET);
-    return res.status(200).json({
-        status: 0,
-        usertoken: signedtoken 
-    })
+});
 
-})
 router.route("/login").post(async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let token;
-    let buyer = await Buyer.findOne({email: email});
-    let vendor = await Vendor.findOne({email: email});
-    if (!buyer && !vendor) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        let token;
+        let buyer = await Buyer.findOne({ email: email });
+        let vendor = await Vendor.findOne({ email: email });
+        if (!buyer && !vendor) {
+            logger.error("Login failed - No such user", { email: email });
+            return res.status(200).json({
+                status: 1,
+                error: "No such user"
+            });
+        }
+        if (buyer) {
+            const verify = await bcrypt.compare(password, buyer.password);
+            if (!verify) {
+                logger.error("Login failed - Incorrect password", { email: email });
+                return res.status(200).json({
+                    status: 1,
+                    error: "Incorrect password"
+                });
+            }
+            token = {
+                email: buyer.email,
+                type: "buyer"
+            };
+        }
+        else {
+            const verify = await bcrypt.compare(password, vendor.password);
+            if (!verify) {
+                logger.error("Login failed - Incorrect password", { email: email });
+                return res.status(200).json({
+                    status: 1,
+                    error: "Incorrect password"
+                });
+            }
+            token = {
+                email: vendor.email,
+                type: "vendor"
+            };
+        }
+        const signedtoken = jwt.sign(token, process.env.SECRET);
+        logger.info("Login successful");
         return res.status(200).json({
+            status: 0,
+            usertoken: signedtoken
+        });
+    } catch (error) {
+        logger.error("Login failed", { error: error.message });
+        return res.status(500).json({
             status: 1,
-            error : "No such user"
-        })
+            error: error.message
+        });
     }
-    if (buyer) {
-        const verify = await bcrypt.compare(password, buyer.password);
-        if (!verify) {
-            return res.status(200).json({
-                status: 1,
-                error : "Incorrect password"
-            })
-        }
-        token = {
-            email: buyer.email,
-            type: "buyer"
-        } 
-    }
-    else {
-        const verify = await bcrypt.compare(password, vendor.password);
-        if (!verify) {
-            return res.status(200).json({
-                status: 1,
-                error : "Incorrect password"
-            })
-        }
-        token = {
-            email: vendor.email,
-            type: "vendor"
-        } 
-    }
-    const signedtoken = jwt.sign(token, process.env.SECRET);
-    return res.status(200).json({
-        status: 0,
-        usertoken: signedtoken 
-    })
 });
-router.route("/register").post(async (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const contact_number = req.body.contact_number;
-    const x = 10
-    const password = await bcrypt.hash(req.body.password, x);
 
-    let duplicatebuyer = await Buyer.findOne({email: email});
-    let duplicatevendor = await Vendor.findOne({email: email});
-    if (duplicatebuyer || duplicatevendor) {
-        return res.status(200).json({
-            status: 1,
-            error: "The email you've entered is already registered"
-        });
-    }
-    if (req.body.type === "buyer") {
-        const age = req.body.age;
-        const batch_name = req.body.batch_name;
-        const wallet = req.body.wallet || 0;
-        const newBuyer = new Buyer({
-            name, 
-            email,
-            age, 
-            contact_number,
-            batch_name,
-            wallet,
-            password
-        });
-        newBuyer.save()
-        .then(() => res.json('Buyer registered!'))
-        .catch(err => res.status(200).json({
-            status: 1,
-            error: err
-        }));
-    }
-    else if (req.body.type === "vendor") {
-        const shop_name = req.body.shop_name;
-        const canteen_timings = {
-            open: moment(req.body.opentiming).format("HH:mm"),
-            close: moment(req.body.closetiming).format("HH:mm")
+router.route("/register").post(async (req, res) => {
+    try {
+        const name = req.body.name;
+        const email = req.body.email;
+        const contact_number = req.body.contact_number;
+        const x = 10;
+        const password = await bcrypt.hash(req.body.password, x);
+
+        let duplicatebuyer = await Buyer.findOne({ email: email });
+        let duplicatevendor = await Vendor.findOne({ email: email });
+        if (duplicatebuyer || duplicatevendor) {
+            logger.error("Registration failed - The email is already registered", { email: email });
+            return res.status(200).json({
+                status: 1,
+                error: "The email you've entered is already registered"
+            });
         }
-        const order_stats = {
-            placed: 0,
-            pending: 0,
-            completed: 0
-        } 
-        const multi = 0;
-        const newVendor = new Vendor({
-            name, 
-            shop_name,
-            email,
-            contact_number, 
-            canteen_timings,
-            order_stats,
-            multi,
-            password
-        });
-        newVendor.save()
-        .then(() => res.json('Vendor registered!'))
-        .catch(err => res.status(200).json({
+        if (req.body.type === "buyer") {
+            const age = req.body.age;
+            const batch_name = req.body.batch_name;
+            const wallet = req.body.wallet || 0;
+            const newBuyer = new Buyer({
+                name,
+                email,
+                age,
+                contact_number,
+                batch_name,
+                wallet,
+                password
+            });
+            newBuyer.save()
+                .then(() => {
+                    logger.info('Buyer registered!');
+                    res.json('Buyer registered!');
+                })
+                .catch(err => {
+                    logger.error('Registration failed - Error registering a new buyer', { error: err.message });
+                    res.status(200).json({
+                        status: 1,
+                        error: err
+                    });
+                });
+        }
+        else if (req.body.type === "vendor") {
+            const shop_name = req.body.shop_name;
+            const canteen_timings = {
+                open: moment(req.body.opentiming).format("HH:mm"),
+                close: moment(req.body.closetiming).format("HH:mm")
+            };
+            const order_stats = {
+                placed: 0,
+                pending: 0,
+                completed: 0
+            };
+            const multi = 0;
+            const newVendor = new Vendor({
+                name,
+                shop_name,
+                email,
+                contact_number,
+                canteen_timings,
+                order_stats,
+                multi,
+                password
+            });
+            newVendor.save()
+                .then(() => {
+                    logger.info('Vendor registered!');
+                    res.json('Vendor registered!');
+                })
+                .catch(err => {
+                    logger.error('Registration failed - Error registering a new vendor', { error: err.message });
+                    res.status(200).json({
+                        status: 1,
+                        error: err
+                    });
+                });
+        }
+    } catch (error) {
+        logger.error('Registration failed', { error: error.message });
+        res.status(500).json({
             status: 1,
-            error: err
-        }));
+            error: error.message
+        });
     }
 });
+
 module.exports = router;
